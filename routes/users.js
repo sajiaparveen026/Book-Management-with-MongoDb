bb5cafcbd078c8bcfc2b612f0fa22d3607f36d7b
sajiaparveen026 <100033460+sajiaparveen026@users.noreply.github.com>
2023-03-15 10:26:29 +0000

    some part remaining

---
+++ routes/users.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+
+//Import Models
+// const BookModel = require("../Models/book-model");
+// const UserModel = require("../Models/user-model");
+const {UserModel , BookModel} = require("../Models/index");
+
+const router = express.Router();
