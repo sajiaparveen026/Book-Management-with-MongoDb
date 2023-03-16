5f55c908d09ef9e3bbd5ee3d2c39fbcbc5571d0a
sajiaparveen026 <100033460+sajiaparveen026@users.noreply.github.com>
2023-03-16 10:31:54 +0000

    done books route

--- controllers/book-controller.js
+++ controllers/book-controller.js
@@ -1,4 +1,6 @@
 const {UserModel , BookModel} = require("../Models/index");
+//const issuedBook = require("../dtos/book-dto");
+const IssuedBook = require("../dtos/book-dto");
 
 exports.getAllBooks = async(req,res) => {
     const books = await BookModel.find();
@@ -39,7 +41,8 @@ exports.getAllIssuedBook = async(req,res) =>{
         issuedBook : {$exists : true}
     }).populate("issuedBook");
 
-    //const issuedBooks = users.map((each)=> new issuedBook(each))
+   //D T O -> data transfer Object
+   const issuedBooks = users.map((each)=> new IssuedBook(each));
     if (issuedBooks.length === 0) {
         return res.status(404).json({
           success: false,
@@ -51,5 +54,37 @@ exports.getAllIssuedBook = async(req,res) =>{
         message: "User With the issued books...",
         data: issuedBook,
       });   
+}
+
+exports.addnewBook = async(req,res) => {
+ const {data} = req.body;
+ if(!data){
+  return res.status(400).json({
+    success:false, message:"No Data to Add A Book",
+  });
+ }
+ await BookModel.create(data);
+ const allBooks = await BookModel.find();
+ 
+  return res.status(201).json({
+    success: true,
+    message: "Book updated", 
+    data: allBooks,
+   });
+}
+
+exports.updateBookById = async(req,res) =>{
+  const {id} = req.params;
+  const { data } = req.body;
+  const updateBook = await BookModel.findOneAndUpdate({
+    _id : id,
+  },data , {
+    new : true,
+  })
+  return res.status(201).json({
+    success: true,
+    message: "Book updated by their id", 
+    data: updateBook,
+   });
 }
 //module.exports = {getAllBooks,getSingleBookById};
--- routes/books.js
+++ routes/books.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {getAllBooks,getSingleBookById,getAllIssuedBook} = require("../controllers/book-controller")
+const {getAllBooks,getSingleBookById,getAllIssuedBook,addnewBook,updateBookById} = require("../controllers/book-controller")
 
 const {UserModel , BookModel} = require("../Models/index");
 const router = express.Router();
@@ -40,30 +40,7 @@ router.get("/:id",getSingleBookById);
      Access : public
      Parameters : none
 */
-router.post("/", (req, res) => {
-  const { id, name, author, genre, price, publisher } = req.body;
-  const book = books.find((each) => each.id === id);
-  if (book) {
-    return res.status(404).json({
-      success: false,
-      message: "Book Exists",
-    });
-  }
-  books.push({
-    id,
-    name,
-    author,
-    genre,
-    price,
-    publisher,
-  });
-
-  return res.status(201).json({
-    success: true,
-    message: "Book updated",
-    data: books,
-  });
-});
+router.post("/", addnewBook);
 
 /*
      Route : /books/:id
@@ -72,30 +49,6 @@ router.post("/", (req, res) => {
      Access : public
      Parameters : id
 */
-router.put("/updateBook/:id", (req, res) => {
-  const { id } = req.params;
-  const { data } = req.body;
-
-  const book = books.find((each) => each.id == id);
-  if (!book) {
-    return res
-      .status(404)
-      .json({ success: false, message: "Book does not exists" });
-  }
-  const updatedBookdata = books.map((each) => {
-    if (each.id == id) {
-      return {
-        ...each,
-        ...data,
-      };
-      return each;
-    }
-  });
-  return res.status(200).json({
-    success: true,
-    message: "Information Updated",
-    data: updatedBookdata,
-  });
-});
+router.put("/updateBook/:id",updateBookById );
 
 module.exports = router;
